4cc2ef5f6528a30001193eabb8311a73f0118365
Fr4nz83 <francesco.lettich@gmail.com>
2025-03-31 11:27:16 +0200

    .

--- pkg/simulator/simulator.go
+++ pkg/simulator/simulator.go
@@ -178,6 +178,9 @@ func New(opts ...Option) (Interface, error) {
 		simontype.PWRScorePluginName: func(configuration runtime.Object, handle framework.Handle) (framework.Plugin, error) {
 			return simonplugin.NewPWRScorePlugin(configuration, handle, &sim.typicalPods)
 		},
+		simontype.PWREXPScorePluginName: func(configuration runtime.Object, handle framework.Handle) (framework.Plugin, error) {
+			return simonplugin.NewPWREXPScorePlugin(configuration, handle, &sim.typicalPods)
+		},
 	}
 
 	// The code below appears to set up the simulator's scheduler framework, specifying the various plugins.
